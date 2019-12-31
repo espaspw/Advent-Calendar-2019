18317f0b5cb0b61be3a2401f4aecb75c4082d81b
Super Panama World <superpanamaworld@gmail.com>
2019-12-31 01:17:05 -0800

    Added final entry

--- src/data/entries/26.js
+++ src/data/entries/26.js
@@ -1,13 +1,11 @@
-const entry = `<p><strong>&ltAdvent Calendar December 30th&gt</strong></p>
-<p>I hope I was able to improve my English skills compared with last year.</p>
-<ol><li><strong>My favourite things:</strong>
-I like traveling. Especially going abroad is so fun since when hearing foreign languages and seeing buildings that are different from Japanese ones, I feel like I&#39;m completely away from the bustle of everyday life. What I mainly do while traveling abroad is visiting museums and eating. 
-I’m also into art. I used to have zero interest on art when I was in my teens. So I couldn’t understand why people bother to waste their time just watching pictures that don’t even move. I majored in economics and politics in university because they looked “practical” and art thingies didn’t seem to have something to do with them. However,  after I took a movie class and watched a lot of movies, I gradually came to admit that art is an essential aspect of human nature. As historically the first drama was played in Greek theatre in front of all the Athenian citizens, human activities inherently consist of art that has imposed its huge influence on our culture since the earliest stage of human era. In short, Now I can enjoy art because I know you have to consider an artwork as a product of relationships between the artist and the world. Plato said something like “Artists are those who interpret the acts of the God since everything on the earth is created by him although the hidden meanings of creatires aren’t visible for most people.” I think you can regard the word “God” he used as the world instead. We are a part of the world, therefore artworks depict us in defferent ways depending on the time, situation, culture, production style, artist’s personal character and so on. Anyway, what I’m trying to say is enjoying art doesn’t mean just standing still in front of the artwork. It’s more than that.</li>
-<li><strong>My new year’s resolution:</strong>
-To be honest, I have never traveled by myself. I didn’t like Onsen(hot springs) so I hardly ever go on a donestic trip other than club camps or school trips. It might be just me, but in my opinion most Japanese people who prefer local trips are obsessed with hot springs, which I used to regard as a waste of time and money as you have a bath in your house. Instead, I wanted to make my trip more active and productive, such as visiting a tourist spot or eating at the popular restaurant.
-As for eating, it’s much less fun than sharing a table with someone because you can’t share food and opinions. (This reminds me of a movie quote, “Happiness becomes real when shared”) Especially for dinner, you can’t choose a fancy restaurant as it makes you feel even lonely. So all things considered, solo trip wasn’t appealing very much.
-Even though, for some reason, I changed my mind out of sudden. It may be because I came to go to the museum and cinema by myself. Now that I think going on a trip by myself is amusing since you don’t have to care about your girlfriend that always complains on your decision and friends that have different taste or thoughts. You can change your plan anytime on a whim, sleep until noon in your bed or read a book in train as much as you want. It’d be appreciated if someone who read this article gave me any recommendations on the destination.</li>
-</ol><p>Lastly, happy new year to everyone in the server! I enjoyed a lot with you all this year. Thank you very much. I hope everything goes well.</p>`
+const entry = `<p><strong>12/31/19</strong></p>
+<p>アドベントカレンダーは大成功だったと思います :roosantayay: 。きょねんは参加者足りなかったから管理するのがなんかむずかしかったけどことしはACの枠が予想より速くうまってしまいました。
+そして、みんなあけめしておめでとうございます :meowawauu: </p>
+<p>最後の参加者として私だけがこれが言えるとおもったからこの機会を逃さないようにしたかった。じつは、今年ぜんぜん日本語を練習していなかった。ステラさんから去年みたいに今年もACの管理する？ってメッセージが見たときにこれに気づいた。返事が書けないみたいな感じだった、日本語の文章の作り方でも忘れたから。:blobcatsipsweats: それで、今年のエントリー、練習するように辞書とか参考しないで身につけた日本語だけで書きたかったんだ。</p>
+<p>さて、言いたいことは私の好きなもの。ちっちゃなころからピアノでのクラシック音楽の練習をしていた。だって数年前もっと即興できるようになりたくてわたしの楽譜の依存を壊したかった。はじめてはこのような弾き方がくるしかった、ときどきぜんぜん上達していないみたいな感じだった。具体的な目標もなかったから練習のやる気がかなり少なかった。いまでもまだうまく即興演奏できないとおもうけどいまさらたいていガクフとかなくても弾けるから結構楽しくなってきた :potatopiano: 。最近ジャズのスタイルの曲などもすきになったからいま他のスタイルをひいてみている。</p>
+<p>好きな音楽っていえば、高校生のとき、うたいてが歌ってみた音楽を紹介された。それとはボカロイドの曲とおなじだけどボカロイドの声のかわりにうたっているのは本物の人間だ。だって、時間と伴ってやっぱりボカロイドの声のほうがもっともすきになった。めぐみさんのおかげでボコトランスの音楽にも興味を引いた。そのスタイルは特別な雰囲気があり、星たちと宇宙のイメージに合っている。いま一番すきなアーティストはkita-keiさん、clean tearsさん、mystekaさん、lemmさんとか。:CatClap: 
+<a href="https://www.nicovideo.jp/watch/sm17322622">https://www.nicovideo.jp/watch/sm17322622</a></p>
+<p>辞書使いずに書くのがむずかしいですね :blobcatsipsweats: このエントリーにはなんども「すきになった」っていうなありふれた表現を使いすぎたなんて :blobjoy: 。もっと詳しく言いたいけど私の語彙だけで説明するのがやっぱり無理でしょう。私来年のAC（いまより勉強したあと）このトピックをもう一回やるかもしれない :mikuyawn:。</p>`
 
 
 
@@ -18,5 +16,15 @@ export default {
   date: 'December 31, 2019',
   lang: 'Japanese',
   avatar: 'espa.gif',
+  emojis: ['roosantayay', 'meowawauu', 'blobcatsipsweats', 
+    {
+      name: 'potatopiano',
+      type: 'gif',
+    }, 
+    {
+      name: 'CatClap',
+      type: 'gif',
+    }, 
+    'blobjoy', 'mikuyawn'],
   entry,
 }
